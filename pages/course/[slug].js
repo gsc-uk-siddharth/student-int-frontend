ef7f305f78fc51ae326945f6d55de3d5af99e282
gsc-uk-siddharth <gscuksiddharth@gmail.com>
2022-12-17 16:31:40 +0530

    Changes done

--- pages/course/[slug].js
+++ pages/course/[slug].js
@@ -104,8 +104,15 @@ const Course = ({ course }) => {
           </div>
         </div>
 
-        <section className="mx-6 py-2 pb-8 lg:ml-36 lg:py-4 lg:pb-16">
+        <section className="mx-6 py-2 pb-8 lg:ml-32 lg:py-4 lg:pb-16">
           <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 lg:text-2xl font-light md:gap-12 md:space-y-0">
+            <div>
+              <div class="flex justify-center items-center mb-4 w-10 h-10  rounded-full bg-gray-900 lg:h-12 lg:w-12 dark:bg-gray-900">
+                <svg class="w-5 h-5 text-primary-100 lg:w-6 lg:h-6 dark:text-primary-100" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"></path></svg>
+              </div>
+              <h3 class="mb-2 text-xl font-bold dark:text-white">Campus</h3>
+              <p class="text-white dark:text-white">{course.location}</p>
+            </div>
             <div>
               <div class="flex justify-center items-center mb-4 w-10 h-10  rounded-full bg-gray-900 lg:h-12 lg:w-12 dark:bg-gray-900">
                 <svg class="w-5 h-5 text-primary-100 lg:w-6 lg:h-6 dark:text-primary-100" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
@@ -206,7 +213,7 @@ const Course = ({ course }) => {
           />
           <Link
             href={`${course.brochureUrl}?dl=course_brochure.pdf`}
-            className="inline-flex items-center justify-center px-20  text-gray-900 lg:px-6 xl:px-24 text-bold ml-6 py-3 xl:ml-36 lg:text-2xl text-sm lg:font-light text-center  border border-gray-200 rounded-lg sm:w-auto hover:bg-cyan-900 hover:text-white focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-white dark:hover:bg-cyan-900 dark:focus:ring-gray-800"
+            className="inline-flex items-center justify-center px-20   lg:px-6 xl:px-24 text-bold ml-6 py-3 xl:ml-36 lg:text-2xl text-sm lg:font-light text-center bg-cyan-900 text-white  border border-gray-200 rounded-lg sm:w-auto hover:bg-cyan-900 hover:text-white focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-white dark:hover:bg-cyan-900 dark:focus:ring-gray-800"
           >
             Download Brochure
           </Link>
