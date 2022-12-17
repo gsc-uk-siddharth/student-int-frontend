2c71ba63d9e4a46b612fcffdf5ff281e0fbb08fc
gsc-uk-siddharth <gscuksiddharth@gmail.com>
2022-12-17 11:14:34 +0530

    fix

--- components/Cards.js
+++ components/Cards.js
@@ -42,6 +42,7 @@ const Cards = ({ schools }) => {
                 </ul>
                 <Link
                   href={`/schools/${school?.slug?.current}`}
+
                   className="inline-flex items-center py-2 my-2 mb-4 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                   Read more
