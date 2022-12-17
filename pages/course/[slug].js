691d4fd095cbe8769eb49851d54d6736b51aa0c5
gsc-uk-siddharth <gscuksiddharth@gmail.com>
2022-12-17 13:26:32 +0530

    fees added in landing pages

--- pages/course/[slug].js
+++ pages/course/[slug].js
@@ -7,6 +7,8 @@ import { createClient } from "next-sanity";
 import Link from "next/link";
 
 
+
+
 const Course = ({ course }) => {
   // First Name	Last Name	Phone Number	Email Address	City	State	Course interested in	Highest Qualification
   const [firstName, setFirstName] = useState("");
@@ -102,28 +104,26 @@ const Course = ({ course }) => {
           </div>
         </div>
 
-
-
-        {/* <section class="bg-transparent  dark:bg-gray-900">
-          <div class="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
-            <dl class="grid max-w-screen-md gap-8 mx-auto text-white sm:grid-cols-3 dark:text-white">
-              <div class="flex flex-col items-center justify-center">
-                <dd class="font-light text-white dark:text-white">Fees</dd>
-                <dt class="mb-2 text-3xl md:text-4xl font-extrabold">
-                  {course.fees}
-                </dt>
+        <section className="mx-6 py-2 pb-8 lg:ml-36 lg:py-4 lg:pb-16">
+          <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 lg:text-2xl font-light md:gap-12 md:space-y-0">
+            <div>
+              <div class="flex justify-center items-center mb-4 w-10 h-10  rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-gray-900">
+                <svg class="w-5 h-5 text-primary-700 lg:w-6 lg:h-6 dark:text-primary-100" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
               </div>
-              <div class="flex flex-col items-center justify-center">
-                <dd class="font-light text-white dark:text-white">
-                  Course Starts
-                </dd>
-                <dt class="mb-2 text-3xl md:text-4xl font-extrabold">
-                  {course.startDate}
-                </dt>
+              <h3 class="mb-2 text-xl font-bold dark:text-white">Fees</h3>
+              <p class="text-white dark:text-white">{course.fees}</p>
+            </div>
+            <div>
+              <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-gray-900">
+                <svg class="w-5 h-5 text-primary-700 lg:w-6 lg:h-6 dark:text-primary-100" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"></path></svg>
               </div>
-            </dl>
+              <h3 class="mb-2 text-xl font-bold dark:text-white">Course Starts</h3>
+              <p class="text-white dark:text-white">{course.startDate}</p>
+            </div>
+
           </div>
-        </section> */}
+        </section>
+
 
         <div className="xl:pl-36 pl-6 lg:text-3xl text-3xl text-white font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
           About the Course
